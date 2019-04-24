d853af43b28f91ec46d11931e474865aa30a0660
cs108 <41218033+cs108@users.noreply.github.com>
2019-04-24 12:54:35 +0800

    Create 3126_bfs.cpp

---
+++ cpp/ACM-ICPC/POJ/Solved/3126_bfs.cpp
@@ -0,0 +1,100 @@
+#include<iostream>
+
+using namespace std;
+
+const int qSize = 1000;
+int que[qSize][2];
+int front=0, rear=0;
+#define QIN(x,step) que[rear][0]=x,que[rear++][1]=step,rear%=qSize
+#define QOUT(x,step) x=que[front][0],step=que[front++][1],front%=qSize
+#define QEMPTY (front%qSize==rear%qSize)
+
+int src, dst;
+int bfs_succ;
+int STEP;
+
+bool isPrime(int x) {
+	bool result = true;
+	if (x == 2)
+		return result;
+	for (int i = 2; i*i <= x; i++)
+	{
+		if (x%i==0)
+		{
+			result = false;
+			break;
+		}
+	}
+	return result;
+}
+
+
+void BFS()
+{
+	int v[10000] = { 0 };
+	front = 0;
+	rear = 0;
+	QIN(src,0);
+	v[src] = 1;
+	bfs_succ = 0;
+	while (!QEMPTY&&!bfs_succ)
+	{
+		int num,step;
+		QOUT(num,step);
+		
+//printf("QOUT(%d,%d)\n", num, step);
+		if (num == dst)
+		{
+			STEP = step;
+			return;
+		}
+		int n;
+		int bit[4];
+		for (int i = 0; i < 4; i++)
+		{
+			int mod = 1;
+			for (int j = i; j >0; j--)
+				mod *= 10;
+			bit[i] = (num/mod)%10;
+		}
+		//change 1
+		
+		for (int i = 0; i < 4; i++)
+		{
+			int bit2[4];
+			for (int l = 0; l < 4; l++)
+				bit2[l] = bit[l];
+			for (int j = 0; j < 10; j++)
+			{
+				if (i == 3 && j == 0)
+					continue;
+				bit2[i] = j;
+
+				//I was trapped by a coding mistake for a long while: n = bit2[0] + bit2[1] * 10 + bit[2] * 100 + bit2[3] * 1000;
+				n = bit2[0] + bit2[1] * 10 + bit2[2] * 100 + bit2[3] * 1000;
+
+				if (v[n] == 0 && isPrime(n))
+				{
+					v[n] = 1;
+					QIN(n, step + 1);
+					//if (n == 1733 || n == 3733 || n== 3739 || n == 3779 || n == 8779 || n == 8179)
+//printf("QIN(%d,%d)\n", n, step + 1);
+				}
+			}
+		}
+	}
+}
+
+int main()
+{
+//	freopen("sample_input.txt", "r", stdin);
+	int T;
+	cin >> T;
+	while (T--)
+	{
+		cin >> src >> dst;
+		BFS();
+		cout << STEP<<endl;
+	}
+//	fclose(stdin);
+}
