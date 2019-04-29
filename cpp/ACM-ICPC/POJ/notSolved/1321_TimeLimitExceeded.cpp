47e49ff27de54c94da583ff7c6c2a9376c28fcb9
cs108 <41218033+cs108@users.noreply.github.com>
2019-04-29 11:44:06 +0800

    Update 1321_TimeLimitExceeded.cpp

--- cpp/ACM-ICPC/POJ/notSolved/1321_TimeLimitExceeded.cpp
+++ cpp/ACM-ICPC/POJ/notSolved/1321_TimeLimitExceeded.cpp
@@ -1,3 +1,9 @@
+/*
+Cut branch "(N - startRow - chessNum < 0)" in DFS;
+added a auxiliary array "visitablePosition";
+added "reverse_mode" to do DFS in a faster way;
+but Still Time Limit Exceeded.
+*/
 #include<iostream>
 
 using namespace std;
@@ -7,6 +13,9 @@ char board[board_size][board_size];
 
 int N, K;
 int solutionNum;
+int visitablePosition[board_size][board_size];
+
+int reverse_mode = 0;
 
 void DFS(int startRow,int visitedColumn[],int chessNum)
 {
@@ -19,20 +28,32 @@ void DFS(int startRow,int visitedColumn[],int chessNum)
 	if (N - startRow - chessNum < 0)
 		return;
 	int r;
-	for (r = startRow; r < N;r++)
+	for (r = startRow; r < N; r++)
 	{
-		for (int c = 0; c < N; c++)
+		for (int i = 0; i < N; i++)
 		{
-			if (board[r][c] == '#'&&visitedColumn[c] == 0)
+			int c = visitablePosition[r][i];
+			if (c == -1)
+				break;
+
+			if (reverse_mode == 0)
 			{
-				int v[board_size]; 
-				for (int i = 0; i < N; i++)
+				if (visitedColumn[c] == 0)
 				{
-					v[i] = visitedColumn[i];
+					int v[board_size];
+					for (int i = 0; i < N; i++)
+					{
+						v[i] = visitedColumn[i];
+					}
+					v[c] = 1;
+					DFS(r, v, chessNum - 1);
 				}
-				v[c] = 1;
-				DFS(r, v, chessNum - 1);
 			}
+			else
+			{
+				DFS(r, visitedColumn, chessNum - 1);
+			}
+
 		}
 	}
 }
@@ -45,16 +66,36 @@ int main()
 		cin >> N >> K;
 		if (N == -1)
 			break;
+
+		reverse_mode = 0;
+		if (K > N / 2&&K<N)
+		{
+			K = N - K;
+			reverse_mode = 1;
+		}
 		for (int r = 0; r < N; r++)
 		{
 			for (int c = 0; c < N; c++)
 			{
+				visitablePosition[r][c] = -1;  //to store visitable column of each row
+			}
+		}
+
+		for (int r = 0,top=0; r < N; r++,top=0)
+		{
+			for (int c = 0; c < N; c++)
+			{
 				cin >> board[r][c];
+				if (board[r][c] == '#')
+				{
+					visitablePosition[r][top++] = c;
+				}
 			}
 		}
 		solutionNum = 0;
 		int visitedCol[board_size] = { 0 };
-		DFS(0, visitedCol, K);
+		if(N>0)
+			DFS(0, visitedCol, K);
 		cout << solutionNum << endl;
 	}
 	//fclose(stdin);
