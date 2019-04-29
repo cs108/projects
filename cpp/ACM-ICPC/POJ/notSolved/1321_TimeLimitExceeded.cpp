75cc698aa9bc34b96bb5c66a3199ffaf387d0e2b
cs108 <41218033+cs108@users.noreply.github.com>
2019-04-29 14:20:41 +0800

    Update 1321_TimeLimitExceeded.cpp

--- cpp/ACM-ICPC/POJ/notSolved/1321_TimeLimitExceeded.cpp
+++ cpp/ACM-ICPC/POJ/notSolved/1321_TimeLimitExceeded.cpp
@@ -1,8 +1,5 @@
 /*
-Cut branch "(N - startRow - chessNum < 0)" in DFS;
-added a auxiliary array "visitablePosition";
-added "reverse_mode" to do DFS in a faster way;
-but Still Time Limit Exceeded.
+Use "printf()" "scanf()" rather than "cin" "cout" still doesn't work.
 */
 #include<iostream>
 
@@ -13,11 +10,8 @@ char board[board_size][board_size];
 
 int N, K;
 int solutionNum;
-int visitablePosition[board_size][board_size];
 
-int reverse_mode = 0;
-
-void DFS(int startRow,int visitedColumn[],int chessNum)
+void DFS(int startRow, int visitedColumn[], int chessNum)
 {
 	if (chessNum == 0)
 	{
@@ -30,30 +24,14 @@ void DFS(int startRow,int visitedColumn[],int chessNum)
 	int r;
 	for (r = startRow; r < N; r++)
 	{
-		for (int i = 0; i < N; i++)
+		for (int c = 0; c < N; c++)
 		{
-			int c = visitablePosition[r][i];
-			if (c == -1)
-				break;
-
-			if (reverse_mode == 0)
+			if (board[r][c] == '#'&&!visitedColumn[c])
 			{
-				if (visitedColumn[c] == 0)
-				{
-					int v[board_size];
-					for (int i = 0; i < N; i++)
-					{
-						v[i] = visitedColumn[i];
-					}
-					v[c] = 1;
-					DFS(r, v, chessNum - 1);
-				}
-			}
-			else
-			{
+				visitedColumn[c] = 1;
 				DFS(r, visitedColumn, chessNum - 1);
+				visitedColumn[c] = 0;
 			}
-
 		}
 	}
 }
@@ -63,40 +41,26 @@ int main()
 	//freopen("sample_input.txt", "r", stdin);
 	while (true)
 	{
-		cin >> N >> K;
+		//cin >> N >> K;
+		scanf("%d %d", &N, &K);
 		if (N == -1)
 			break;
-
-		reverse_mode = 0;
-		if (K > N / 2&&K<N)
-		{
-			K = N - K;
-			reverse_mode = 1;
-		}
+		int n = N;
 		for (int r = 0; r < N; r++)
 		{
-			for (int c = 0; c < N; c++)
-			{
-				visitablePosition[r][c] = -1;  //to store visitable column of each row
-			}
-		}
-
-		for (int r = 0,top=0; r < N; r++,top=0)
-		{
-			for (int c = 0; c < N; c++)
+			/*for (int c = 0; c < N; c++)
 			{
 				cin >> board[r][c];
-				if (board[r][c] == '#')
-				{
-					visitablePosition[r][top++] = c;
-				}
-			}
+			}*/
+			scanf("%s", board[r]);
 		}
+		N = n;
 		solutionNum = 0;
 		int visitedCol[board_size] = { 0 };
 		if(N>0)
 			DFS(0, visitedCol, K);
-		cout << solutionNum << endl;
+		//cout << solutionNum << endl;
+		printf("%d\n", solutionNum);
 	}
 	//fclose(stdin);
 	return 0;
