8364645f0a06d5825bb7a9058a564e57eb7420c7
cs108 <41218033+cs108@users.noreply.github.com>
2019-04-29 13:29:45 +0800

    Update 3009_dfs.cpp

--- cpp/ACM-ICPC/POJ/Solved/3009_dfs.cpp
+++ cpp/ACM-ICPC/POJ/Solved/3009_dfs.cpp
@@ -52,8 +52,8 @@ void DFS(int r, int c, int time, int (*m)[map_size])
 			{
 				int r3 = r2 + dir[i][0];
 				int c3 = c2 + dir[i][1];
-													//I wrote "if (r3<0 || r3>R || c3<0 || c3>C)" and of course failed
-													//This is really a deadly mistake but not hard to avoid
+											//I wrote "if (r3<0 || r3>R || c3<0 || c3>C)" and of course failed         
+											//This is really a deadly mistake but not hard to avoid
 				if (r3<0 || r3>=R || c3<0 || c3>=C)  //(r3,c3):front 2 step square    
 				{
 					break;
@@ -62,7 +62,7 @@ void DFS(int r, int c, int time, int (*m)[map_size])
 				{
 					map[r3][c3] = vacant_square;
 					DFS(r3 - dir[i][0], c3 - dir[i][1], time + 1, map);
-					map[r3][c3] = block;
+					map[r3][c3] = block;       //this step is so important that I've been trapped for a long time without this statement         
 					break;
 				}
 				if (map[r3][c3] == goal)
